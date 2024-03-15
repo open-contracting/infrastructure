e5572abd51fa5ea10194fd945a37e32d43946a9f
Duncan Dewhurst <duncan.dewhurst@opendataservices.coop>
2024-03-15 14:39:22 +1300

    docs/conf.py: Copy sustainability.yaml to _static

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ html_favicon = '_static/favicon-16x16.ico'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static', 'examples']
+html_static_path = ['_static', 'examples', '../mapping/sustainability.yaml']
 
 
 # -- Local configuration -----------------------------------------------------
