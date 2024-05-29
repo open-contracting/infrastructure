54058547909cc3dce782e1ffe24152abbc167aa4
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-05-28 21:35:55 -0400

    ci: Use Biome defaults (double quoting, trailing comma, semi-colons)

--- docs/_static/json-example-format.js
+++ docs/_static/json-example-format.js
@@ -1,58 +1,58 @@
 /* global renderjson */
 
-for (const element of document.querySelectorAll('.expandjson')) {
-  const defaultOpen = []
-  let fileClassName
+for (const element of document.querySelectorAll(".expandjson")) {
+  const defaultOpen = [];
+  let fileClassName;
   for (const className of element.classList) {
-    if (className.substring(0, 7) === 'expand-' && className.length > 7) {
-      defaultOpen.push(className.substring(7))
+    if (className.substring(0, 7) === "expand-" && className.length > 7) {
+      defaultOpen.push(className.substring(7));
     }
-    if (className.substring(0, 5) === 'file-') {
-      fileClassName = className
+    if (className.substring(0, 5) === "file-") {
+      fileClassName = className;
     }
   }
 
-  let data = JSON.parse(element.textContent)
+  let data = JSON.parse(element.textContent);
   // If the jsoninclude directive indexed to a JSON array (a common mistake), only display the first entry.
   if (data.length) {
-    data = data[0]
+    data = data[0];
   }
 
-  const replacement = renderjson.set_show_to_level(1).set_max_string_length(100).set_default_open(defaultOpen)(data)
+  const replacement = renderjson.set_show_to_level(1).set_max_string_length(100).set_default_open(defaultOpen)(data);
   // element.firstElementChild.replaceWith(replacement) // https://caniuse.com/mdn-api_parentnode_firstelementchild
-  element.replaceChild(replacement, element.querySelector('.highlight-json'))
+  element.replaceChild(replacement, element.querySelector(".highlight-json"));
 
-  const container = element.previousElementSibling
-  let select
-  if (container?.classList.contains('selection-container')) {
+  const container = element.previousElementSibling;
+  let select;
+  if (container?.classList.contains("selection-container")) {
     // Hide additional examples.
-    element.style.display = 'none'
-    container.appendChild(element)
+    element.style.display = "none";
+    container.appendChild(element);
 
     // Display the select element if there are multiple options.
-    select = container.querySelector('select')
-    select.style.display = ''
+    select = container.querySelector("select");
+    select.style.display = "";
   } else {
-    const div = document.createElement('div')
-    element.insertAdjacentElement('beforebegin', div)
-    div.className = 'selection-container'
-    div.appendChild(element)
+    const div = document.createElement("div");
+    element.insertAdjacentElement("beforebegin", div);
+    div.className = "selection-container";
+    div.appendChild(element);
 
-    select = document.createElement('select')
-    div.insertAdjacentElement('afterbegin', select)
+    select = document.createElement("select");
+    div.insertAdjacentElement("afterbegin", select);
 
     // Hide the select element if there is one option.
-    select.style.display = 'none'
-    select.addEventListener('change', () => {
-      for (const child of div.querySelectorAll('.expandjson')) {
-        child.style.display = 'none'
+    select.style.display = "none";
+    select.addEventListener("change", () => {
+      for (const child of div.querySelectorAll(".expandjson")) {
+        child.style.display = "none";
       }
-      div.querySelector(`.${select.value}`).style.display = ''
-    })
+      div.querySelector(`.${select.value}`).style.display = "";
+    });
   }
 
-  const option = document.createElement('option')
-  option.value = fileClassName
-  option.textContent = fileClassName.replace('file-', '')
-  select.appendChild(option)
+  const option = document.createElement("option");
+  option.value = fileClassName;
+  option.textContent = fileClassName.replace("file-", "");
+  select.appendChild(option);
 }
