3f3342674d57752a926ca305943d2bc8eddfa6ca
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-10-06 21:31:43 -0400

    chore: Add noqa comment

--- tests/__init__.py
+++ tests/__init__.py
@@ -16,7 +16,7 @@ test_navigation_params = [
 test_search_params = [
     ("en", r"found \d+ page\(s\) matching"),
     # See https://github.com/sphinx-doc/sphinx/issues/11008
-    # > ('es', r'encontraron \d+ páginas que coinciden'),
+    # ('es', r'encontraron \d+ páginas que coinciden'),  # noqa: ERA001
 ]
 
 last_path = "/support/"
